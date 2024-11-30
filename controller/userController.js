29d662f2e1a40a380259dbe2f29dc077ae78aaee
ihtisham914 <shaami.khn321@gmail.com>
2024-11-30 14:30:21 +0500

    added check for email must be unique

--- controller/userController.js
+++ controller/userController.js
@@ -12,10 +12,18 @@ const JWT_SECRET = process.env.JWT_SECRET
 export const signupUser = async (req, res) => {
     const { name, email, password } = req.body
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
     try {
+        const existingUser = await UserModel.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                message: "User already exists",
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
         const user = await UserModel.create({ name, email, password: hashedPassword })
 
         const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
